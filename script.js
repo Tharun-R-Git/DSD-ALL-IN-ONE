f1731446e2789ab44fee0375c563f7a1ec7698b4
Tharun-R-Git <118326822+Tharun-R-Git@users.noreply.github.com>
2023-11-13 15:36:06 +0530

    added the files

---
+++ script.js
@@ -0,0 +1,9 @@
+
+<button id="openPopup">Verilog code for 4 BIT ALU</button>
+
+<div id="popup" class="popup">
+  <span class="close" id="closePopup">&times;</span>
+  <img src="your-image.jpg" alt="Pop-up Image" class="popup-image" id="popupImage">
+</div>
+
+<script src="script.js"></script>
